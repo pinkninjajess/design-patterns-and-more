95fa496e01413f1c179643d36663392e145fec49
Jessica Valenti <jessica.valenti1@gmail.com>
2021-03-11 12:09:18 -0500

    Updated wording for clarity

--- src/after/strategy/MallardDuck.java
+++ src/after/strategy/MallardDuck.java
@@ -4,7 +4,7 @@ public class MallardDuck extends Duck {
 
     public MallardDuck() {
         // note: not best practice to program to an implementation as is done below... better to use dependency injection
-        // in this example, we are setting the behaviours to concrete classes - can be changed at runtime
+        // in this example, we are setting the behaviours to concrete classes, which can easily be changed at runtime
         quackBehaviour = new Quack();
         flyBehaviour = new FlyWithWings();
     }
